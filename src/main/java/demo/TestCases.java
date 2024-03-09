2056a11cdda1c8e5b0e043588ebb451364038047
Saisandhiya <saisandhiya02@gmail.com>
2024-03-10 01:30:22 +0530

    ME_QA_XLEETCODE1_MODULE_L1

--- src/main/java/demo/TestCases.java
+++ src/main/java/demo/TestCases.java
@@ -64,20 +64,49 @@ public class TestCases {
     public void testCase02() throws InterruptedException {
         System.out.println("Start Test case: testCase02");
         driver.get("https://leetcode.com/");
-        String title = driver.getCurrentUrl();
-        String expected = "leetcode";
-        if (title.contains(expected)) {
-            System.out.println("Title contains " + expected);
-        }
+
         WebElement questionLink = driver.findElement(By.xpath("//*[@id='product']/div/div/div[1]/div/a/p"));
         // p[@class='link']/p[text()='View Questions ']
         questionLink.click();
         String problems = driver.getCurrentUrl();
-        String expected1 = "problemset";
-        if (problems.contains(expected1)) {
-            System.out.println("Title contains " + expected1);
+        String expected0 = "problemset";
+        if (problems.contains(expected0)) {
+            System.out.println("Title contains " + expected0);
         }
-
+        WebElement firstQues = driver.findElement(By.xpath("//div[@role='cell']/div/div/div/div/a[text()='Two Sum']"));
+        String title1 = firstQues.getText();
+        String expected1 = "Two Sum";
+        if (title1.contains(expected1)) {
+            System.out.println("1. " + expected1);
+        }
+        WebElement secondQues = driver
+                .findElement(By.xpath("//div[@role='cell']/div/div/div/div/a[text()='Add Two Numbers']"));
+        String title2 = secondQues.getText();
+        String expected2 = "Add Two Numbers";
+        if (title2.contains(expected2)) {
+            System.out.println("2. " + expected2);
+        }
+        WebElement thirdQues = driver.findElement(By.xpath(
+                "//div[@role='cell']/div/div/div/div/a[text()='Longest Substring Without Repeating Characters']"));
+        String title3 = thirdQues.getText();
+        String expected3 = "Longest Substring Without Repeating Characters";
+        if (title3.contains(expected3)) {
+            System.out.println("3. " + expected3);
+        }
+        WebElement fourthQues = driver
+                .findElement(By.xpath("//div[@role='cell']/div/div/div/div/a[text()='Median of Two Sorted Arrays']"));
+        String title4 = fourthQues.getText();
+        String expected4 = "Median of Two Sorted Arrays";
+        if (title4.contains(expected4)) {
+            System.out.println("4. " + expected4);
+        }
+        WebElement fifthQues = driver
+                .findElement(By.xpath("//div[@role='cell']/div/div/div/div/a[text()='Longest Palindromic Substring']"));
+        String title5 = fifthQues.getText();
+        String expected5 = "Longest Palindromic Substring";
+        if (title5.contains(expected5)) {
+            System.out.println("5. " + expected5);
+        }
         System.out.println("end Test case: testCase02");
     }
 
