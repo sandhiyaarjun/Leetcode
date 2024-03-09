ac8bb295eea8df10c93ffa543eb81c5d00c4fb59
Saisandhiya <saisandhiya02@gmail.com>
2024-03-10 00:44:55 +0530

    ME_QA_XLEETCODE1_MODULE_L1

--- src/main/java/demo/App.java
+++ src/main/java/demo/App.java
@@ -1,21 +1,21 @@
 package demo;
-import java.net.MalformedURLException;
 
+import java.net.MalformedURLException;
 
 public class App {
     public void getGreeting() throws InterruptedException, MalformedURLException {
-        
+
         // This is to remove unnecessary warnings from your console
         System.setProperty("java.util.logging.config.file", "logging.properties");
-        
+
         TestCases tests = new TestCases(); // Initialize your test class
 
-        //TODO: call your test case functions one after other here
+        // TODO: call your test case functions one after other here
 
         tests.testCase01();
+        tests.testCase02();
 
-        //END Tests
-
+        // END Tests
 
         tests.endTest(); // End your test by clearning connections and closing browser
     }
--- src/main/java/demo/TestCases.java
+++ src/main/java/demo/TestCases.java
@@ -9,15 +9,21 @@ import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
 import java.util.logging.Level;
 import io.github.bonigarcia.wdm.WebDriverManager;
-
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import java.time.Duration;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class TestCases {
     ChromeDriver driver;
-    public TestCases()
-    {
+
+    public TestCases() {
         System.out.println("Constructor: TestCases");
 
-        WebDriverManager.chromedriver().timeout(30).setup();
+        WebDriverManager.chromedriver().browserVersion("122.0.6261.95").setup();
         ChromeOptions options = new ChromeOptions();
         LoggingPreferences logs = new LoggingPreferences();
 
@@ -33,24 +39,46 @@ public class TestCases {
 
         // Set browser to maximize and wait
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
 
     }
 
-    public void endTest()
-    {
+    public void endTest() {
         System.out.println("End Test: TestCases");
         driver.close();
         driver.quit();
 
     }
 
-    
-    public  void testCase01(){
+    public void testCase01() throws InterruptedException {
         System.out.println("Start Test case: testCase01");
-        driver.get("https://www.google.com");
-        System.out.println("end Test case: testCase02");
+        driver.get("https://leetcode.com/");
+        String title = driver.getCurrentUrl();
+        String expected = "leetcode";
+        if (title.contains(expected)) {
+            System.out.println("Title contains " + expected);
+        }
+        System.out.println("end Test case: testCase01");
     }
 
+    public void testCase02() throws InterruptedException {
+        System.out.println("Start Test case: testCase02");
+        driver.get("https://leetcode.com/");
+        String title = driver.getCurrentUrl();
+        String expected = "leetcode";
+        if (title.contains(expected)) {
+            System.out.println("Title contains " + expected);
+        }
+        WebElement questionLink = driver.findElement(By.xpath("//*[@id='product']/div/div/div[1]/div/a/p"));
+        // p[@class='link']/p[text()='View Questions ']
+        questionLink.click();
+        String problems = driver.getCurrentUrl();
+        String expected1 = "problemset";
+        if (problems.contains(expected1)) {
+            System.out.println("Title contains " + expected1);
+        }
+
+        System.out.println("end Test case: testCase02");
+    }
 
 }
